0eaf3f26d3fa4c884eed7cbc60e568d63fd81e8d
amraj9 <amritar2@uic.edu>
2024-04-02 12:55:22 -0500

    Edits

--- Visualization2.py
+++ Visualization2.py
@@ -4,9 +4,31 @@ import matplotlib.pyplot as plt
 
 postcovid_df = pd.read_csv('csv_files/Crimes_2021_to_Present.csv')
 
-crime_counts = postcovid_df.groupby(['RegionName', 'Primary Type']).size().reset_index(name='Count')
+selected_neighborhoods2 = [
+        "Armor Square",
+        "Bridgeport",
+        "East Garfield Park",
+        "Hermosa",
+        "Humboldt Park",
+        "Lincoln Park",
+        "Logan Square",
+        "Loop",
+        "Lower West Side",
+        "McKinley Park",
+        "Near North Side",
+        "Near South Side",
+        "Near West Side",
+        "North Lawndale",
+        "South Lawndale",
+        "West Garfield Park",
+        "West Town",
+    ]
 
-crime_by_location = postcovid_df.groupby('RegionName').size().reset_index(name='TotalCount')
+filteredpostcovid_df = postcovid_df[postcovid_df['RegionName'].isin(selected_neighborhoods2)]
+
+crime_counts = filteredpostcovid_df.groupby(['RegionName', 'Primary Type']).size().reset_index(name='Count')
+
+crime_by_location = filteredpostcovid_df.groupby('RegionName').size().reset_index(name='TotalCount')
 
 crime_with_totals = crime_counts.merge(crime_by_location, on='RegionName')
 
@@ -19,7 +41,7 @@ sns.heatmap(pivot_table, annot=False, cmap='viridis', linecolor='white', linewid
 plt.title('Percentage Distribution of Crime Types by Location')
 plt.ylabel('Neighborhoods')
 plt.xlabel('Primary Type')
-plt.xticks(rotation=45)
+plt.xticks(rotation=85, fontsize = 8)
 plt.yticks(fontsize=10)
 plt.tight_layout()
 plt.show()
