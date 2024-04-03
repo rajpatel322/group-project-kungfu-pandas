d6b71797228484fc1668ecaf298b2f87255968cc
amraj9 <amritar2@uic.edu>
2024-04-02 21:30:12 -0500

    Heatmap edits

--- Visualization2.py
+++ Visualization2.py
@@ -5,23 +5,23 @@ import matplotlib.pyplot as plt
 postcovid_df = pd.read_csv('csv_files/Crimes_2021_to_Present.csv')
 
 selected_neighborhoods2 = [
-        "Armor Square",
-        "Bridgeport",
-        "East Garfield Park",
-        "Hermosa",
-        "Humboldt Park",
-        "Lincoln Park",
-        "Logan Square",
+        "Near West Side",
+        "West Town",
         "Loop",
-        "Lower West Side",
-        "McKinley Park",
         "Near North Side",
         "Near South Side",
-        "Near West Side",
+        "Lower West Side",
+        "Armor Square",
+        "East Garfield Park",
         "North Lawndale",
         "South Lawndale",
+        "Humboldt Park",
+        "Bridgeport",
+        "McKinley Park",
         "West Garfield Park",
-        "West Town",
+        "Logan Square",
+        "Lincoln Park",
+        "Hermosa",
     ]
 
 filteredpostcovid_df = postcovid_df[postcovid_df['RegionName'].isin(selected_neighborhoods2)]
@@ -36,8 +36,15 @@ crime_with_totals['Percentage'] = (crime_with_totals['Count'] / crime_with_total
 
 pivot_table = crime_with_totals.pivot(index='RegionName', columns= 'Primary Type', values = 'Percentage')
 
+overall = crime_with_totals.groupby('Primary Type')['Count'].sum() / crime_with_totals.groupby('Primary Type')['TotalCount'].sum()
+overall = overall.reset_index(name='OverallPercentage')
+
+sorted = overall.sort_values(by='OverallPercentage', ascending=False)['Primary Type']
+
+pivot_table = pivot_table[sorted]
+
 plt.figure(figsize=(14,10))
-sns.heatmap(pivot_table, annot=False, cmap='viridis', linecolor='white', linewidths=0.1)
+sns.heatmap(pivot_table, annot=False, cmap='viridis', linecolor='white', linewidths=0.05)
 plt.title('Percentage Distribution of Crime Types by Location')
 plt.ylabel('Neighborhoods')
 plt.xlabel('Primary Type')
